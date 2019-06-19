efaa72a969a6718682fdb78ac64291a01bc0b049
Bas Verweij <basverweij@users.noreply.github.com>
2019-06-19 22:19:53 +0200

    Added ITypeResolver interface.

---
+++ src/OffSync.Apps.Codegen.Application/Interfaces/ITypeResolver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using OffSync.Apps.Codegen.Domain.ApplicationModels;
+
+namespace OffSync.Apps.Codegen.Application.Interfaces
+{
+    public interface ITypeResolver
+    {
+        Type Resolve(
+            ApplicationModel application,
+            string type);
+    }
+}
